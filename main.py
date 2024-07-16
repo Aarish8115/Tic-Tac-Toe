d89a159cbe4225994a413d5d957aaf30fce1e5cc
Aarish <mohdaarish127@gmail.com>
2024-07-16 13:05:58 +0530

    added comments

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tkinter
 
-def new_game():
+def new_game():# starts a new game
     global current,inputs
     current=inputs[0]
     playerLabel.config(text=current+"'s Turn")
@@ -8,7 +8,7 @@ def new_game():
         for column in range(3):
             buttons[row][column].config(text="")
 
-def turn(row,column):
+def turn(row,column):#processes the user input and changes UI for the next move
     global current
     if buttons[row][column]['text']=="" and check_status()==False:
         buttons[row][column]['text']=current
@@ -21,7 +21,7 @@ def turn(row,column):
             playerLabel.config(text="Tie")
 
 
-def space():
+def space():#checks if any empty spaces are left on the board
     ct=9
     for row in range(3):
         for column in range(3):
@@ -31,7 +31,7 @@ def space():
         return False
 
 
-def check_status():
+def check_status():#checks for win or a tie after every move
     for row in range(3):
         if buttons[row][0]['text']==buttons[row][1]['text']==buttons[row][2]['text']!="":
             return True
@@ -50,7 +50,7 @@ def check_status():
         return False
 
 
-def change_player():
+def change_player(): #changes player after every move
     global current,inputs
     if current==inputs[0]:
         current=inputs[1]
@@ -61,37 +61,24 @@ def change_player():
 
 window=tkinter.Tk()
 window.title("Tic Tac Toe")
-inputs=["X","O"]
-current="X"
+inputs=["X","O"]#the two players
+current=inputs[0]#active player
 buttons=[[None,None,None],
          [None,None,None],
-         [None,None,None]]
+         [None,None,None]]#board
 
 playerLabel=tkinter.Label(text=current+"'s turn",font=(50))
 playerLabel.pack()
 
-
 frame=tkinter.Frame(window,border=20)
 frame.pack()
 
-for row in range(3):
+for row in range(3):#buttons on the board
     for column in range(3):
         buttons[row][column]=tkinter.Button(frame,text="",width=10,height=5,command=lambda row=row,column=column:turn(row,column))
         buttons[row][column].grid(row=row,column=column)
 
-
-
 restartbtn=tkinter.Button(text="Restart",font=(40),padx=10,pady=5,command=new_game)
-restartbtn.pack()
-
-
-
-
-
-
-
-
-
-
+restartbtn.pack()#restarts the game
 
 window.mainloop()
